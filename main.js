d64580dbfdab2616d10a4bf21debfc18b1b4dda8
suvarna-yanumala <yanamalasuvarna@gmail.com>
2020-06-10 13:35:57 +0530

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
-import {addUser,getUsers} from './user/user'
+import http from 'http'
+import socketio from 'socket.io'
+
+import {addUser,removeUser,getUser,getUserInRoom} from './user/user'
 
 const PORT = 5000;
 let app = express()
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = socketio(server);
 let routers = express.Router();
 
 app.use(bodyParser.json());
@@ -20,25 +23,48 @@ routers.get('/', function (req, res) {
     res.sendFile('index.html');
 })
 
-io.on('connection', socket => {
-	console.log('a user connected');
+io.on('connection', (socket) => {
 
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
+	socket.on('join', ({name,room},callback) => {
+		const{error,user} = addUser({id: socket.id, name, room });
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}.`});
+
+    socket.broadcast.to(user.room).emit('message',{user: 'admin', text: `${user.name} has joined!`})
+
+    io.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)});
+
+    callback();
+		
 	});
 
-	socket.on('message', message => {
-		console.log('message: ' + message);
-		//Broadcast the message to everyone
-		io.emit('message', message);
-	});
+	socket.on('sendMessage', (message,callback) =>{
+		const user = getUser(socket.id);
+	
+		io.to(user.room).emit('message', {user: user.name, text: message});
+		// io.to(user.room).emit('roomData', {room: user.room, text: message});
+	
+		callback();
+	  });
+	
+	  socket.on('disconnect',()=>{
+		const user = removeUser(socket.id);
+		if(user){
+		  io.to(user.room).emit('message',{user: 'admin', text: `${user.name} has left`});
+		  io.to(user.room).emit('roomData', {room: user.room, users: getUserInRoom(user.room)});
+		}
+	  })
 });
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
 
-routers.post("/api/reister", addUser);
-// routers.get("/api/login", signin);
-routers.get("/api/getUsers", getUsers);
+// routers.post("/api/reister", addUser);
+// // routers.get("/api/login", signin);
+// routers.get("/api/getUsers", getUsers);
--- user/user.js
+++ user/user.js
@@ -1,56 +1,72 @@
 
 import getConnection from '../db'
+// let user = []
 
-export const addUser = async (req,res) => {
-    console.log("request parameters ::::::",JSON.stringify(req.body))
-    let json = JSON.parse (JSON.stringify(req.body))
-    let db = await getConnection()
-    db.collection('user').insert(json,{ unique: true }).then((result) => {
-            console.log("Record added :- " + JSON.stringify(result));
-            if(result!=null){
-                res.json({status:"success",message:"record inserted successfully"})
-                res.end()
-            }else{
-                res.json({status:"failed",message:"record not inserted"})
-                res.end()
-            }
-        });
-    
-}
-// export const signin = async(req,res) => {
+// export const addUser = async (req,res) => {
 //     console.log("request parameters ::::::",JSON.stringify(req.body))
 //     let json = JSON.parse (JSON.stringify(req.body))
 //     let db = await getConnection()
-//     if(json.username != null && json.passwprd != null){
-//         db.collection('user').find(json).then((result) => {
+//     db.collection('user').insert(json,{ unique: true }).then((result) => {
 //             console.log("Record added :- " + JSON.stringify(result));
 //             if(result!=null){
-//                 res.json({status:"success",message:"login successfully"})
-//                 res.end(); 
+//                 res.json({status:"success",message:"record inserted successfully"})
+//                 res.end()
 //             }else{
-//                 res.json({status:"failed",message:"record not founded"})
-//                 res.end();
+//                 res.json({status:"failed",message:"record not inserted"})
+//                 res.end()
 //             }
 //         });
-
-//     }else{
-//         res.json({status:"failed",message:"username and password should not be null"})
-//         res.end(); 
-//     }
     
 // }
 
-export const getUsers = async(req,res) => {
-    let db = await getConnection()
-    db.collection('user').find({}).toArray((err,result) => {
-        console.log("Record added :- " + JSON.stringify(result));
-        if (err) throw err;
-        if(result){
-            res.json({status:"success",message:result})
-            res.end(); 
-        }else{
-            res.json({status:"failed",message:{}})
-            res.end();
-        }
-    });
+// export const getUsers = async(req,res) => {
+//     let db = await getConnection()
+//     db.collection('user').find({}).toArray((err,result) => {
+//         console.log("Record added :- " + JSON.stringify(result));
+//         if (err) throw err;
+//         if(result){
+//             res.json({status:"success",message:result})
+//             res.end(); 
+//         }else{
+//             res.json({status:"failed",message:{}})
+//             res.end();
+//         }
+//     });
+// }
+
+const users = [];
+
+export const addUser = ({id,name,room})=>{ 
+
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingUser = users.find((user)=> user.name === name && user.room === room);
+
+  if(!name||!room) return {error: 'Username and room are required.'}
+  
+  if(existingUser){
+    return {error: 'Username is taken'};
+  }
+
+  const user = { id,name,room };
+
+  users.push(user);
+
+  return {user}
 }
+
+export const removeUser = (id)=>{
+  const index = users.findIndex((user)=> user.id===id);
+  
+  if(index!==-1){
+    return users.splice(index,1)[0];
+  }
+}
+
+export const getUser = (id)=>{
+  let result = users.find(user=> user.id===id);
+  return result;
+}
+
+export const getUserInRoom = (room)=> users.filter((user)=> user.room===room );
